c4496c36e930189e3717ea6628334277986617c6
Sam Lorimer <slorimerjr@gmail.com>
2022-04-12 21:39:14 +0000

    final

--- main.py
+++ main.py
@@ -36,30 +36,22 @@ capitals = {
 }
 
 #Nesting a List in a Dictionary
+# Challenge: change the France entry so it contains another dictionary. For the key, use 'cities_visited' and for the value, keep the cities listed.
 
 travel_log = {
-  "France": ["Paris", "Lille", "Dijon"],
-  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+  "France": {"cities_visted": ["Paris", "Lille", "Dijon"], "total_visits": 12 },
+  "Germany": {"cities_visited":["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5 },
 }
-
 #Nesting Dictionary in a Dictionary
+
 
 travel_log = {
   "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
   "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
 }
-
 #Nesting Dictionaries in Lists
 
 travel_log = [
-{
-  "country": "France", 
-  "cities_visited": ["Paris", "Lille", "Dijon"], 
-  "total_visits": 12,
-},
-{
-  "country": "Germany",
-  "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-  "total_visits": 5,
-},
+{"country": "France", "cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12,},
+{"country": "Germany","cities_visited": ["Berlin", "Hamburg", "Stuttgart"],"total_visits": 5,},
 ]
